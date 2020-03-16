7c8c48d94c2a13e61c2dddc14e704b194a332fea
Huang-Jin <1309740625@qq.com>
2020-03-16 10:47:27 +0800

    fix a bug with consumer.

--- ProducerCosumerDemo/main.cpp
+++ ProducerCosumerDemo/main.cpp
@@ -16,7 +16,7 @@ void consumer(CBlockQueue* bq) {
 	//CBlockQueue* bq = static_cast<CBlockQueue*>(arg);
 
 	while (bq->available()) {
-		int data;
+		int data = -1;
 		bq->pop(data);
 		cout << "data comsumed done: " << data << ", trhead id: " << this_thread::get_id() << "\n";
 		//sleep(0.5);
@@ -46,7 +46,7 @@ int main() {
 	thread th_prod(producer, &bqueue, 0, 100);
 
 	vector<thread> th_cons;
-	const int nums = 2;
+	const int nums = 5;
 	for (int i = 0; i < nums; i++) {
 		th_cons.emplace_back(consumer, &bqueue);
 	}
